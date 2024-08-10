4f17e626f867fca1b63cd4cf5e471967f1cfa6de
Josiah Della Foresta <43655839+Joss98@users.noreply.github.com>
2024-08-10 11:32:06 -0400

    Create Player object with initial variable values

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Create Player object with initial variable values
+const Player = {
+    netWorth: 0,
+    income: 0,
+    savingsRate: 0,
+    expenseRate: 0,
+    taxRate: 0,
+    interestRate: 0,
+    yearsPlayed: 0,
+    futureValueAnnuity: 0,
+};
