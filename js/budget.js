f14284a633a1e9a72e3ce2734681c520e8962e5b
Josiah Della Foresta <43655839+Joss98@users.noreply.github.com>
2024-08-11 20:08:46 -0400

    Move budget categories to new budget.js file

---
+++ js/budget.js
@@ -0,0 +1,19 @@
+// Contains all logic related to handling the budget UI
+
+
+
+
+
+
+
+// Define budget categories
+
+const budgetCategories = {
+    housing: 0,
+    education: 0,
+    family: 0,
+    transport: 0,
+    discretionary: 0,
+    savings: 0,
+    debtRepayment: 0
+}
--- js/financial.js
+++ js/financial.js
@@ -6,18 +6,6 @@
 
 
 
-// Define budget categories
-
-const budgetCategories = {
-    housing: 0,
-    education: 0,
-    family: 0,
-    transport: 0,
-    discretionary: 0,
-    savings: 0,
-    debtRepayment: 0
-}
-
 // Calculate the future value of an annuity
 
 function calculateFutureValueAnnuity(P, r, n) {
